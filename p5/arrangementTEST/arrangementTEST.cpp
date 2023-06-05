6467e0bf6c269101bd1ebd1af070cd36a7192f7f
DESKTOP-K0AAPHD\KGA <dltjdwo123654@gmail.com>
2023-06-05 18:13:32 +0900

    다섯쨋날

---
+++ p5/arrangementTEST/arrangementTEST.cpp
@@ -0,0 +1,72 @@
+﻿// arrangementTEST.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <stdlib.h>
+#include <conio.h>
+#include <windows.h>
+
+using namespace std;
+
+void Description005()
+{
+	//길을 걷는 사람1
+	/*char board[5] = { 0, };*/
+
+	//{ board를 초기화 하는 로직
+
+	char board[5];
+
+	int whileValue = 0;
+
+	while (true)
+	{
+		for (int i = 0; i < 5; i++)
+		{
+			board[i] = '*';
+
+			if (i == whileValue)
+			{
+				board[i] = '0';
+			}
+		}
+		//{ board를 초기화 하는 로직
+		for (int i = 0; i < 5; i++)
+		{
+			printf("%c", board[i]);
+		}
+
+		printf("\n\n");
+		whileValue++;
+		_getch();
+		//} board를 초기화하는 로직
+	}
+}
+
+//void Description004();
+//{  
+//	int numbers[9]; 
+//
+//
+//	//구구단 출력하는 프로그램 만들기
+//	int lineCount[9] = { 0, };
+//
+//	int lineCount[9] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+//
+//	printf("배열의 값 앞은 (인덱스, 뒤는 값) : (%d, %d) \n", 1, numbers[1]);
+//	printf("배열의 값 앞은 (인덱스, 뒤는 값) : (%d, %d) \n", 2, numbers[2]);
+//	printf("배열의 값 앞은 (인덱스, 뒤는 값) : (%d, %d) \n", 3, numbers[3]);
+//	printf("배열의 값 앞은 (인덱스, 뒤는 값) : (%d, %d) \n", 4, numbers[4]);
+//	printf("배열의 값 앞은 (인덱스, 뒤는 값) : (%d, %d) \n", 5, numbers[5]);
+//	printf("배열의 값 앞은 (인덱스, 뒤는 값) : (%d, %d) \n", 6, numbers[6]);
+//	printf("배열의 값 앞은 (인덱스, 뒤는 값) : (%d, %d) \n", 7, numbers[7]);
+//	printf("배열의 값 앞은 (인덱스, 뒤는 값) : (%d, %d) \n", 8, numbers[8]);
+//	printf("배열의 값 앞은 (인덱스, 뒤는 값) : (%d, %d) \n", 9, numbers[9]);
+//
+//}
+
+int main()
+{
+	Description005();
+}
+
---
+++ p5/char char/char char.cpp
@@ -0,0 +1,60 @@
+﻿// char char.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+
+
+void Description003()
+{
+	
+	//배열이란 무엇인지 실습 배열은 여러가지 변수를 쓸떄 사용 ex) 총게임에서 총알 하나당 변수로 5개를 만든다.
+	//초기화는 알수있는 값으로 설정하기 위해서 초기화 한다.
+	int numbers[3000];     //5개의 변수를 선언한 것이다., 배열 초기화 첫번쨰
+	//numbers[0] = 100;
+	/*numbers[1] = 200;
+	numbers[2] = 300;
+	numbers[3] = 400;
+	numbers[4] = 500;*/
+
+
+	const int MAX_ARRAY = 5;
+	for (int i = 0; i < MAX_ARRAY; i++)
+	{
+		numbers[i] = (i + 1) * 100;
+		printf("배열의 값 앞은 (인덱스, 뒤는 값) : (%d, %d) \n", i, numbers[i]);
+
+	}
+
+	//배열 초기화 두번째
+	int numbers2[5] = { 10,20,30,40,50 };
+
+	//배열 초기화 세번쨰
+	int numbers3[5] = { 0, };
+
+	//printf("배열의 값 앞은 (인덱스, 뒤는 값) : (%d, %d) \n", 0, numbers[0]);
+	//printf("배열의 값 앞은 (인덱스, 뒤는 값) : (%d, %d) \n", 1, numbers[1]);
+	//printf("배열의 값 앞은 (인덱스, 뒤는 값) : (%d, %d) \n", 2, numbers[2]);
+	//printf("배열의 값 앞은 (인덱스, 뒤는 값) : (%d, %d) \n", 3, numbers[3]);
+	//printf("배열의 값 앞은 (인덱스, 뒤는 값) : (%d, %d) \n", 4, numbers[4]);
+
+}
+
+
+void Description002()
+{
+	// 캐스팅 테스트 형변환
+	char char_ = 'A';
+	int int_ = char_;
+	float floatValue = 100.123;
+	int_ = (int)floatValue;
+	printf("int_변수 안에 무슨 값이 들어있나? %f \n",(float) int_);
+
+}
+
+
+int main()
+{
+	Description002();
+	Description003();
+}
